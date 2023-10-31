9a25a5981001ba75b615efa4d09557e982ba1b0b
Rob Flynn <rjflynn2@shef.ac.uk>
2023-10-31 12:44:52 +0000

    load config

---
+++ stfc/utils/general.py
@@ -0,0 +1,7 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from omegaconf import OmegaConf
+
+def load_config(config: str = "paths.yaml"):
+    config = OmegaConf.load(config)
+    return config
